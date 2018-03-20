afae2883fedc457df8562c1520a4a66d498eb42a
ppgallas <ppgallas@gmail.com>
2018-03-20 11:37:30 +0100

    Added gui class to main file

--- gui.py
+++
@@ -1,8 +0,0 @@
-import Tkinter
-
-
-root = Tkinter.Tk()
-root.title('Image Scraper 1.0')
-
-root.mainloop()
-
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from bs4 import BeautifulSoup as bs
 import requests
 import time
 import os
+import Tkinter as tk
 
-url = raw_input('Enter URL:')
-images = []
 
-#This function will get the address of website and scroll it automatically
-def get_page():
+
+
+def get_page(url):
+    images=[]
+
     global driver
     driver = webdriver.Chrome()
     driver.get(url)
@@ -20,39 +22,56 @@ def get_page():
             break
         else:
             last_height = new_height
-get_page()
+
 
 #This function uses BeautifulSoup to parse through the page source and find images.
-def get_img():
+    def get_img():
 
-    sp = bs(driver.page_source, 'html.parser')
-    for image in sp.find_all('img'):
-        images.append(image)
-get_img()
+        sp = bs(driver.page_source, 'html.parser')
+        for image in sp.find_all('img'):
+            images.append(image)
+    get_img()
 
-#Create folder which will contain downloaded images.
-def make_dir():
-    if not os.path.exists('Downloaded images'):
-        os.mkdir('Downloaded images')
-    os.chdir('Downloaded images')
-make_dir()
+    #Create folder which will contain downloaded images.
+    def make_dir():
+        if not os.path.exists('Downloaded images'):
+            os.mkdir('Downloaded images')
+        os.chdir('Downloaded images')
+    make_dir()
 
-#Function which saves images.
-def save_img():
+    #Function which saves images.
+    def save_img():
 
-    x = 0
+        x = 0
 
-    for image in images:
-        try:
-            url = image['src']
-            source = requests.get(url)
-            with open('img-{}.jpg'.format(x), 'wb') as f:
-                f.write(requests.get(url).content)
-                x += 1
-        except:
-            print 'Error while saving image.'
-save_img()
+        for image in images:
+            try:
+                url = image['src']
+                source = requests.get(url)
+                with open('img-{}.jpg'.format(x), 'wb') as f:
+                    f.write(requests.get(url).content)
+                    x += 1
+            except:
+                print 'Error while saving image.'
+    save_img()
+
+class App(tk.Frame):
+    def __init__(self,master=None,**kw):
+        tk.Frame.__init__(self,master=master,**kw)
+        self.txtURL = tk.StringVar()
+        self.entryURL = tk.Entry(self,textvariable=self.txtURL)
+        self.entryURL.grid(row=0,column=0)
+        self.btnGet = tk.Button(self,text="SCRAPE",command=self.getImgs)
+        self.btnGet.grid(row=0,column=2)
+
+    def getImgs(self):
+        get_page(self.txtURL.get())
 
 
+root = tk.Tk()
+root.title('Image Scraper 1.0')
+tk.Label(root, text='Enter URL:').grid(row=0)
+App(root).grid()
+root.mainloop()
 
 
